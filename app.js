aa93459944b0b2b4580dc6b1bc249c42819679e6
Dmitrij <dimaparfenyk1990@gmail.com>
2024-09-18 12:50:31 +0300

    add server on new branch

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-
-import contactsRouter from "./routes/contactsRouter.js";
+const express = require("express");
+const cors = require("cors");
+const contactsRouter = require("./routes/contactRouter.js");
 
 const app = express();
 
-app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
-app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
 });
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
+app.use((error, req, res, next) => {
+  const { status = 500, message = "Server error" } = error;
   res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+module.exports = app;
---
+++ schemas/index.js
@@ -0,0 +1,1 @@
+const addSchema = required("./addSchema.js");
